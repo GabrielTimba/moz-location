3cf217e9bc6a508401195781cdda91e937d9bc42
Gabriel Timba <gabrieltimba@hotmail.com>
2021-06-01 12:06:22 +0200

    update route

--- start/routes.ts
+++ start/routes.ts
@@ -1,7 +1,12 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async () => {
-  return { hi: 'welcome to mozlocation api' }
+  return {
+    'hi': 'welcome to mozlocation api',
+    '/provinces': 'return list of provinces',
+    '/provinces/:code': 'return a specific province',
+    '/districts?code=xx-x': 'return a list of districts of a certain province',
+  }
 })
 
 Route.get('/provinces', 'ProvincesController.index')
